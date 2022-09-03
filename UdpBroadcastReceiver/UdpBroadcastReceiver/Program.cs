4c3fe1a765ad3d3b2898e8418370cf462dfc2436
Helen <86743390+RedK0i@users.noreply.github.com>
2022-09-03 20:48:57 +0300

    Add files via upload

---
+++ UdpBroadcastReceiver/UdpBroadcastReceiver/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UdpBroadcastReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket sockBroadcastReceiver = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp); 
+            IPEndPoint ipepLocal = new IPEndPoint(IPAddress.Any, 23000);
+            byte[] buffer = new byte[512];
+            int nCountReceived= 0;
+            string textReceived = string.Empty;
+            IPEndPoint ipepSender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint epSender = (EndPoint)ipepSender;
+            try
+            {
+                sockBroadcastReceiver.Bind(ipepLocal);
+                while (true)
+                {
+                    nCountReceived = sockBroadcastReceiver.ReceiveFrom(buffer, ref epSender);
+                    textReceived = Encoding.ASCII.GetString(buffer, 0, nCountReceived);
+                    Console.WriteLine("Number of Bytes Received: " + nCountReceived);
+                    Console.WriteLine("Text Received: " + textReceived);
+                    Console.WriteLine("Received From: " + epSender.ToString());
+                    if (textReceived.Equals("<ECHO>"))
+                    {
+                        sockBroadcastReceiver.SendTo(buffer, 0, nCountReceived, SocketFlags.None, epSender);
+                        Console.WriteLine("Text Echoed back...");
+                    }
+                    Array.Clear(buffer, 0, buffer.Length);
+
+                }
+
+            }
+            catch (Exception excp)
+            {
+
+                Console.WriteLine(excp.ToString());
+            }
+        }
+    }
+}
---
+++ UdpBroadcastSender/UdpBroadcastSender/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+namespace UdpBroadcastSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket sockBroadcaster = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            sockBroadcaster.EnableBroadcast = true;
+            IPEndPoint broadcastEP = new IPEndPoint(IPAddress.Parse("255.255.255.255"),23000);
+            byte[] buffer = new byte[] {0x0D,0X0A};
+            string strUserInput = string.Empty;
+            IPEndPoint ipepSender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint epSender = (EndPoint)ipepSender;
+            string textReceived = string.Empty;
+            int nCountReceived = 0;
+            try
+            {
+                sockBroadcaster.Bind(new IPEndPoint(IPAddress.Any,0));
+                while (true)
+                {
+                    Console.Write("Please input a string to send broadcast type <EXIT> to close");
+                    strUserInput = Console.ReadLine();
+                    if (strUserInput.Equals("<EXIT>"))
+                    {
+                        break;
+                    }
+
+                    buffer = Encoding.ASCII.GetBytes(strUserInput);
+                    sockBroadcaster.SendTo(buffer, broadcastEP);
+                    if (strUserInput.Equals("<ECHO>"))
+                    {
+                        nCountReceived = sockBroadcaster.ReceiveFrom(buffer, ref epSender);
+                        textReceived = Encoding.ASCII.GetString(buffer, 0, nCountReceived);
+                        Console.WriteLine("Number of Bytes Received: " + nCountReceived);
+                        Console.WriteLine("Text Received: " + textReceived);
+                        Console.WriteLine("Received From: " + epSender.ToString());
+                        Array.Clear(buffer, 0, buffer.Length);
+                    }
+                }
+                sockBroadcaster.Shutdown(SocketShutdown.Both);
+                sockBroadcaster.Close();
+
+            }
+            catch (Exception excp)
+            {
+
+                Console.WriteLine(excp.ToString());
+            }
+        }
+    }
+}
